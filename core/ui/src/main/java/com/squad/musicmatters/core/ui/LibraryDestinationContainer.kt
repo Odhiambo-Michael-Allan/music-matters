ef7d717a166111e767fe91b04a6779909d577753
Odhiambo-Michael-Allan <odhiambomichaelallan@gmail.com>
2026-07-09 15:27:13 +0300

    Latest Changes

--- core/ui/src/main/java/com/squad/musicmatters/core/ui/LibraryDestinationContainer.kt
+++ core/ui/src/main/java/com/squad/musicmatters/core/ui/LibraryDestinationContainer.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +17,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import com.squad.musicmatters.core.designsystem.component.MusicMattersIcons
 
-@OptIn( ExperimentalMaterial3Api::class )
+@OptIn(  ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class )
 @Composable
 fun LibraryDestinationContainer(
     @StringRes titleResId: Int? = null,
@@ -52,7 +54,7 @@ fun LibraryDestinationContainer(
             modifier = Modifier.fillMaxSize()
         ) {
             if ( isLoading )
-                CircularProgressIndicator(
+                LoadingIndicator(
                     modifier = Modifier.align( androidx.compose.ui.Alignment.Center )
                 )
             else content()
